3b5cae82486eb438fd92efa1ab2c4642ba77b4e3
SergioDN91 <sergiodn917@gmail.com>
2026-08-07 01:31:29 -0400

    Desarrollo hito 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import Navbar from './components/Navbar';
-import Home from './components/Home';
 import Footer from './components/Footer';
+// import Home from './components/Home';
+// import RegisterPage from './components/RegisterPage';
+import LoginPage from './components/LoginPage';
+
 import './App.css'
 
 function App() {
   return (
     <div className="d-flex flex-column min-vh-100">
       <Navbar />
-      <Home />
+      {/* <Home /> */}
+      {/* <RegisterPage /> */}
+      <LoginPage />
       <Footer />
     </div>
   )
---
+++ src/components/LoginPage.jsx
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+
+const LoginPage = () => {
+  
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  
+  const handleLogin = (e) => {
+    e.preventDefault();
+
+    
+    if (!email.trim() || !password.trim()) {
+      alert('Authentication failed: All fields are required!');
+      return;
+    }
+
+    
+    if (password.length < 6) {
+      alert('Authentication failed: Password must be at least 6 characters!');
+      return;
+    }
+
+    alert('Authentication successful!');
+    
+    setEmail('');
+    setPassword('');
+  };
+
+  return (
+    <div className="container d-flex flex-column align-items-center justify-content-center my-5 flex-grow-1">
+      <form onSubmit={handleLogin} className="w-100" style={{ maxWidth: '500px' }}>
+        <h2 className="fs-1 fw-bold mb-4 text-start">Login</h2>
+        
+        <div className="mb-3">
+          <label className="form-label fs-5 text-muted">Email</label>
+          <input
+            type="email"
+            className="form-control form-control-lg"
+            placeholder="Enter your email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+
+        <div className="mb-4">
+          <label className="form-label fs-5 text-muted">Password</label>
+          <input
+            type="password"
+            className="form-control form-control-lg"
+            placeholder="Enter your password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+
+        <button type="submit" className="btn btn-primary btn-lg px-4">Login</button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginPage;
